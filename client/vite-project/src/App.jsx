14bd1e44d9d9fba94c90c3acabba8a5c0ed13366
h1dd3ngod <71835824+h1dd3ngod@users.noreply.github.com>
2023-05-09 21:26:25 +0300

    style: room design settled

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -4,13 +4,23 @@ import "./styles/App.sass";
 function App() {
     return (
         <>
-			<h1>Collaborative design</h1>
-            <fragment className="app room">
-				<div className="toolbar">
-
-				</div>
-				<div className="canvas">
-
+            <fragment className="app">
+				<div className="room">
+					<aside className="toolbar">
+						<h2>Toolbar</h2>
+						<div>
+							<p>item 1</p>
+							<p>item 2</p>
+						</div>
+					</aside>
+					<div className="canvas-container">
+						<div className="setting-bar">
+							<h2>Setting bar</h2>
+						</div>
+						<div className="canvas">
+							<canvas /* width={600} height={600} */></canvas>
+						</div>
+					</div>
 				</div>
 			</fragment>
         </>
