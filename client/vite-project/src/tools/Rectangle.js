f53613d6203ee010f8760dde655040a0a5c5e2fd
h1dd3ngod <71835824+h1dd3ngod@users.noreply.github.com>
2023-05-10 01:32:21 +0300

    feat: shift key make the square

---
+++ client/vite-project/src/tools/Rectangle.js
@@ -0,0 +1,54 @@
+import Tool from "./Tool";
+
+export default class Rectangle extends Tool {
+    constructor(canvas) {
+        super(canvas);
+        this.listen();
+    }
+
+    listen() {
+        this.canvas.onmousemove = this.mouseMoveHandler.bind(this);
+        this.canvas.onmousedown = this.mouseDownHandler.bind(this);
+        this.canvas.onmouseup = this.mouseUpHandler.bind(this);
+    }
+
+    mouseUpHandler(e) {
+        this.mouseDown = false;
+    }
+
+    mouseDownHandler(e) {
+        this.mouseDown = true;
+        this.ctx.beginPath();
+        this.startX = e.pageX - e.target.offsetLeft;
+        this.startY = e.pageY - e.target.offsetTop
+        this.drawn = this.canvas.toDataURL()
+    }
+
+    mouseMoveHandler(e) {
+        if (this.mouseDown) {
+            let currentX = e.pageX - e.target.offsetLeft;
+            let currentY = e.pageY - e.target.offsetTop;
+            let height = currentY - this.startY;
+            let width = currentX - this.startX;
+            if (e.shiftKey) {
+                let longerSide = height > width ? height : width
+                height = longerSide
+                width = longerSide
+            }
+            
+            this.draw(this.startX, this.startY, width, height);
+        }
+    }
+
+    draw(x, y, w, h) {
+        const img = new Image()
+        img.src = this.drawn
+        img.onload = () => {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+            this.ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height)
+            this.ctx.strokeRect(x,y,w,h)
+            // this.ctx.fill()
+        }
+        console.log( w, h)
+    }
+}
