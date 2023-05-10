0abd1c3b8e3e63f28dbd6b5a3d34af1ec02cca52
h1dd3ngod <71835824+h1dd3ngod@users.noreply.github.com>
2023-05-10 16:39:15 +0300

    feat: routing main

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,22 +1,52 @@
-import {useState } from "react";
+import React, {useState } from "react";
 import "./styles/App.sass";
 import Toolbar from "./components/Toolbar"
 import Canvas from "./components/Canvas"
 import SettingBar from "./components/SettingBar";
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 
 function App() {
     return (
-        <>
-            <div className="app">
-				<div className="room">
-					<Toolbar/>
-					<div className="canvas-container">
-						<SettingBar/>
-						<Canvas/>
-					</div>
-				</div>
-			</div>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/:id/*"
+                    element={
+                        <div className="app">
+                            <div className="room">
+                                <Toolbar />
+                                <div className="canvas-container">
+                                    <SettingBar />
+                                    <Canvas />
+                                </div>
+                            </div>
+                        </div>
+                    }
+                />
+                <Route
+                    path="/*"
+                    element={<Navigate to={`f${(+new Date()).toString(16)}`} />}
+                />
+            </Routes>
+        </BrowserRouter>
+        // <BrowserRouter>
+        //     <div className="app">
+        //         <Routes>
+        //             <Route path="/:id/*">
+        //                 <div className="room">
+        //                     <Toolbar />
+        //                     <div className="canvas-container">
+        //                         <SettingBar />
+        //                         <Canvas />
+        //                     </div>
+        //                 </div>
+        //             </Route>
+        //             <Route path="/*">
+        //                 <Navigate to={`f${(+new Date()).toString(16)}`} />
+        //             </Route>
+        //         </Routes>
+        //     </div>
+        // </BrowserRouter>
     );
 }
 
